343d91b145634db84081d8d4425d18c89dc0b5e0
sameep-s <sameep.sharma98@gmail.com>
2022-05-31 23:09:35 +0530

    rightsidebar

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -1,6 +1,7 @@
 import { faBell, faComment, faShareNodes, faHeart, faBookmark, faFeatherPointed, faHashtag, faHome, faImage, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import RightSidebar from '../../components/Right_sidebar/RightSidebar';
 import './homePage.css';
 
 const HomePage = () => {
@@ -97,46 +98,7 @@ const HomePage = () => {
                         </div>
                     </div>
 
-                    <aside className='right__side__items'>
-                        <div className="right__side__container__main flex a-item-center flex-col">
-                            <div className="right__side__search__bar">
-                                <input type="text" placeholder='Enter the UserName to Search' className='input-txt' name="" id="" />
-                            </div>
-
-                            <div className="container__followSuggestions flex flex-col a-item-center jc-center">
-                                <div className="heading__followSuggestions flex a-item-center">
-                                    <span>Who To Follow</span>
-                                    <button className="show__more__suggestions btn btn-primary">
-                                        Show More
-                                    </button>
-                                </div>
-
-                                <div className="user__list__item__container p-1">
-                                    <div className="user__listItem flex">
-                                        <div className="user__listItem__img">
-                                            <img
-                                                className='avatar'
-                                                src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
-                                                alt="user__avatar"
-                                            />
-                                        </div>
-
-
-
-                                        <div className="user__listItem__info__container flex">
-                                            <div className="user__listItem__info">
-                                                <div className="user__listItem__name">Sameep Sharma</div>
-                                                <div className="user__listItem__userName txt-gray">@_sameep_</div>
-                                            </div>
-
-                                            <button className='btn btn-primary'>Follow+</button>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </aside>
+                    <RightSidebar />
 
                 </main>
             </div >
