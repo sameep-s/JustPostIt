b3c7812380d455e68ae1baafb4652bf675e3c27e
sameep-s <sameep.sharma98@gmail.com>
2022-06-04 20:04:31 +0530

    added homePage Route.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages';
 
 function App() {
   return (
     <>
       <HomePage />
+      <Routes>
+        <Route path='/home' element={<HomePage />} />
+      </Routes>
     </>
   );
 }
