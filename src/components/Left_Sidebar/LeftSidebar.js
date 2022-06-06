6657f1ce316350be619c1185f8ca4d337f151184
sameep-s <sameep.sharma98@gmail.com>
2022-06-06 20:40:44 +0530

    added navlink

--- src/components/Left_Sidebar/LeftSidebar.js
+++ src/components/Left_Sidebar/LeftSidebar.js
@@ -2,18 +2,33 @@ import React from 'react';
 import { faArrowRightFromBracket, faBell, faBookmark, faFeatherPointed, faHashtag, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './leftSidebar.css';
+import { Link, NavLink } from 'react-router-dom';
 
 
 const LeftSidebar = () => {
+
+
+    function styleActiveNav({ isActive }) {
+        return {
+            fontWeight: isActive ? "bolder" : "",
+            color: isActive ? "var(--primary)" : "",
+            backgroundColor: isActive ? "blue" : "",
+        }
+    }
+
     return (
         <>
             <aside className='container__main__sidebar flex flex-col pos-stick'>
                 <div className="icon__sidebar">
-                    <FontAwesomeIcon icon={faFeatherPointed} ></FontAwesomeIcon>
+                    <Link to={'/home'}>
+                        <FontAwesomeIcon icon={faFeatherPointed} ></FontAwesomeIcon>
+                    </Link>
                 </div>
                 <div className='sidebar__links flex flex-col'>
                     <ul>
-                        <li> <div> <FontAwesomeIcon icon={faHome} /> <span className='link__text'>Home</span> </div></li>
+                        <NavLink to={"/home"} style={styleActiveNav}>
+                            <li> <div> <FontAwesomeIcon icon={faHome} /> <span className='link__text'>Home</span> </div></li>
+                        </NavLink>
                         <li> <div> <FontAwesomeIcon icon={faHashtag}></FontAwesomeIcon> <span className='link__text'>Explore</span> </div></li>
                         <li> <div> <FontAwesomeIcon icon={faBookmark}></FontAwesomeIcon> <span className='link__text'>Bookmarks</span> </div></li>
                         <li> <div> <FontAwesomeIcon icon={faBell}></FontAwesomeIcon> <span className='link__text'>Notification</span> </div></li>
