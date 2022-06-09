ed2a57771279bf7a4403aae6ed91abc148521c75
sameep-s <sameep.sharma98@gmail.com>
2022-06-09 14:55:42 +0530

    redux store and slice.

---
+++ src/features/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    encodedToken: null,
+    user: null,
+    isLoggedIn: false,
+}
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        logout: (state) => {
+            localStorage.removeItem('tokenSocial');
+            state = initialState;
+
+            return state;
+        }
+    }
+})
